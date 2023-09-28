7a75e890260feec45e8e97d67214f70a8ffbf374
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 17:09:34 +0800

    modify

--- lesson12_2.py
+++ lesson12_2.py
@@ -9,29 +9,40 @@ class Player:
         self.__dice4 = random.randint(1,6)
 
     def __play() -> int:
-        value=''
+        value = 0
         pair = 0
-        score_t = 0 
+        value_t = 0 
         n = sorted([self.__dice1, self.__dice2, self.__dice3, self.__dice4])
-        for x in range(4):
-            for j in range(x+1,4) :
-                if n[x] == n[j] :
-                    t += 1
-                    score_t += n[j]
-        if pair == 1 :
-            value = sum(n)-score_t*2
-        elif  pair == 2:
-            value = max(n)*2
-        elif  pair == 6:
-            value = max(n)*100
-        else :
-            value = 0  
+        while value == 0:
+            for x in range(4):
+                for j in range(x+1,4) :
+                    if n[x] == n[j] :
+                        pair += 1
+                        value_t += n[j]
+            if pair == 1 :
+                value = sum(n)-value_t*2
+            elif  pair == 2:
+                value = max(n)*2
+            else:
+                if pair == 6 and self.__dice1 == 1:
+                    value = 13
+                if pair == 6 and self.__dice1 == 2:
+                    value = 14
+                if pair == 6 and self.__dice1 == 3:
+                    value = 15
+                if pair == 6 and self.__dice1 == 4:
+                    value = 16
+                if pair == 6 and self.__dice1 == 5:
+                    value = 17
+                if pair == 6 and self.__dice1 == 6:
+                    value = 18
+                else :
+                    value = 0  
         return value
 
     @property
     def value(self) -> int:
-        #呼叫self.__play()
-        pass
+        self.__play()
 
     def __repr__(self) -> str:
         descript = ""
