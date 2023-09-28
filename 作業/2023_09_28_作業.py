ae93d22ec0adb63d19c9dbc55edb9c7d5504473f
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 19:43:58 +0800

    modify

---
+++ 作業/2023_09_28_作業.py
@@ -0,0 +1,15 @@
+import random
+class Player:
+#attribute
+    def __init__(self,name:str):
+        self.name = name
+        self.__dice1 = random.randint(1,6)
+        self.__dice2 = random.randint(1,6)
+        self.__dice3 = random.randint(1,6)
+        self.__dice4 = random.randint(1,6)
+#method
+    def __play():
+        pass
+#property
+    def value(self):
+        return __play()
