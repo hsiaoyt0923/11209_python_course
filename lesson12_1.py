6a1d2a056c59dead3bbe65a35d1940431f2454d9
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 15:15:08 +0800

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -23,6 +23,7 @@ class Person:
         return round(self.weight / (self.height*0.01)**2, ndigits=2)
 
 
+# __name__指這份文件；__main__是預設名稱
 if __name__ == '__main__':
     p1 = Person('Eddie',60,175)
     print(p1.name)
