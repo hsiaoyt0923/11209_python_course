5e70bbee5492d6db083f7a72e0154f96645d18fe
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 13:00:51 +0800

    modify

--- lesson11_3.py
+++ lesson11_3.py
@@ -1,15 +1,18 @@
 class Person:
-    def __init__(self,name:str, weight:int, height:int):
+    def __init__(self,name:str, weight:int, height:int):                         #attribute
         self.name = name
         self.weight = weight
         self.height = height
 
     def __str__(self): 
         return f'name:{self.name}\nweight:{self.weight}\nheight:{self.height}'
-    
-    #method
-    def bmi(self):
+        
+    def bmi(self):                                                               #method
         return round(self.weight / (self.height*0.01)**2, ndigits=2)
+
+
+
+
 
 
 if __name__ == '__main__':
