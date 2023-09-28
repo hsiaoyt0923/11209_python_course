9151535a781b9fb7c66b14579af8a07fdfd7b629
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 16:54:25 +0800

    modify

--- lesson12_2.py
+++ lesson12_2.py
@@ -2,10 +2,31 @@ import random
 
 class Player:
     def __init__(self, name:str):
-        pass
+        self.name = name
+        self.__dice1 = random.randint(1,6)
+        self.__dice2 = random.randint(1,6)
+        self.__dice3 = random.randint(1,6)
+        self.__dice4 = random.randint(1,6)
 
     def __play() -> int:
-        pass
+        value=''
+        pair = 0
+        score_t = 0 
+        n = sorted([self.__dice1, self.__dice2, self.__dice3, self.__dice4])
+        for x in range(4):
+            for j in range(x+1,4) :
+                if n[x] == n[j] :
+                    t += 1
+                    score_t += n[j]
+        if pair == 1 :
+            value = sum(n)-score_t*2
+        elif  pair == 2:
+            value = max(n)*2
+        elif  pair == 6:
+            value = max(n)*100
+        else :
+            value = 0  
+        return value
 
     @property
     def value(self) -> int:
@@ -15,11 +36,11 @@ class Player:
     def __repr__(self) -> str:
         descript = ""
         descript += "徐國堂\n"
-        descript += "骰子1=5\n"
-        descript += "骰子2=3\n"
-        descript += "骰子3=5\n"
-        descript += "骰子4=5\n"
-        dsecript += "得分=15分"
+        descript += f"骰子1:{self.__dice1}\n"
+        descript += f"骰子2:{self.__dice2}\n"
+        descript += f"骰子3:{self.__dice3}\n"
+        descript += f"骰子4:{self.__dice4}\n"
+        dsecript += f"得分:{value}分"
         return descript
     
 if __name__ == "__main__":
