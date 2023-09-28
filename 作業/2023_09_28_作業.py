a2e7c2451bb3a0e3b49035d855dfc6b8d712ca81
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 23:43:37 +0800

    ver1.0

--- 作業/2023_09_28_作業.py
+++ 作業/2023_09_28_作業.py
@@ -1,61 +1,59 @@
 import random
+
 class Player:
 #attribute
     def __init__(self, name:str):
         self.name = name
-        value = ''
-        while value == '':
-            self.__dice1 = random.randint(1,6)
-            self.__dice2 = random.randint(1,6)
-            self.__dice3 = random.randint(1,6)
-            self.__dice4 = random.randint(1,6)
 #method
-        def __play(self) -> int:
-            D = sorted([self.__dice1, self.__dice2, self.__dice3, self.__dice4])
+    def __play(self):
+        score = ''
+        while score == '':   
+            dice1 = random.randint(1,6)
+            dice2 = random.randint(1,6)
+            dice3 = random.randint(1,6)
+            dice4 = random.randint(1,6) 
+            D = sorted([dice1, dice2, dice3, dice4])
             if (D[0] == D[1] == D[2] == D[3]):
                 if D[0] == 1:
-                    value = 13
+                    score = 13
                 if D[0] == 2:
-                    value = 14
+                    score = 14
                 if D[0] == 3:
-                    value = 15
+                    score = 15
                 if D[0] == 4:
-                    value = 16
+                    score = 16
                 if D[0] == 5:
-                    value = 17
+                    score = 17
                 if D[0] == 6:
-                    value = 18
+                    score = 18
             elif (D[0] != D[1] != D[2] != D[3]):
-                value = ''
+                score = ''
             elif (D[0] == D[1] == D[2]) or (D[1] == D[2] == D[3]): 
-                value = ''
+                score = ''
             else:
                 if D[0] == D[1]:
-                    value = D[2] + D[3]
+                    score = D[2] + D[3]
                 elif D[1] == D[2]:
-                    value = D[0] + D[3]
+                    score = D[0] + D[3]
                 elif D[2] == D[3]:
-                    value = D[0] + D[1] 
-        return value
+                    score = D[0] + D[1] 
+        
+        return f'骰子一:{dice1}\n骰子二:{dice2}\n骰子三:{dice3}\n骰子四:{dice4}\n得分:{score}'
+
 #property
     @property
-    def value(self) ->int:
+    def value(self):
         return self.__play()
-    
+
+#被呼叫時傳出字串
     def __repr__(self) -> str:
-        descript = ""
-        descript += f"姓名:{self.name}\n"
-        descript += f"骰子1:{self.__dice1}\n"
-        descript += f"骰子2:{self.__dice2}\n"
-        descript += f"骰子3:{self.__dice3}\n"
-        descript += f"骰子4:{self.__dice4}\n"
-        dsecript += f"得分:{self.__play()}分"
+        descript = f"姓名:{self.name}"
         return descript
 
 p1 = Player('甲')
-print(p1.value)
 print(p1)
-
+print(p1.value)
+print()
 p2 = Player('乙')
-print(p2.value)
 print(p2)
+print(p2.value)
