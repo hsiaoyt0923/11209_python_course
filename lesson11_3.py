05de4ba56c6a198f5063cf078fe04a8d910f37c7
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 12:04:37 +0800

    modify

--- lesson11_3.py
+++ lesson11_3.py
@@ -4,9 +4,15 @@ class Person:
         self.weight = weight
         self.height = height
 
-    def __str__(self):
+    def __str__(self): 
         return f'name:{self.name}\nweight:{self.weight}\nheight:{self.height}'
+    
+    #method
+    def bmi(self):
+        return round(self.weight / (self.height*0.01)**2, ndigits=2)
+
 
 if __name__ == '__main__':
-    p1 = Person('Eddie',26,175)
+    p1 = Person('Eddie',60,175)
     print(p1)
+    print('BMI:',p1.bmi())
