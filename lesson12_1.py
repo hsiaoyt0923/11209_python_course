4ccd601bac8927385feea2e89148b4dc0747e059
Eddie Hsiao <xcvb1321@gmail.com>
2023-09-28 15:12:09 +0800

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -1,24 +1,29 @@
 class Person:
 #attribute
-    def __init__(self,n:str, weight:int, height:int):                         
+    def __init__(self,n:str, weight:int, height:int):
         self.__name = n
         self.weight = weight
         self.height = height
 
 #property
     @property
-    def name(self):
+    def name(self) -> str: 
         return self.__name
+    
+    @property
+    def getBMI(self) -> float:
+        return self.bmi()
 
-    def __str__(self): 
+
+    def __str__(self) -> str:
         return f'name:{self.__name}\nweight:{self.weight}\nheight:{self.height}'
 
 #method       
-    def bmi(self):                                                               
+    def bmi(self):
         return round(self.weight / (self.height*0.01)**2, ndigits=2)
 
 
 if __name__ == '__main__':
     p1 = Person('Eddie',60,175)
     print(p1.name)
-    print('BMI:',p1.bmi())
+    print('BMI:',p1.getBMI)
