e9e4160a3c377c5796849bafc152da8b02bc906d
Eddie Hsiao <xcvb1321@gmail.com>
2023-10-16 12:07:24 +0800

    modify

--- lesson18/index.py
+++ lesson18/index.py
@@ -1,17 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 class Window(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.geometry('400x250+300+300')
-        self.title('Lines')
+        self.title('Image')
         self.configure(background='#F4A7B9')
     
 class MyFrame(tk.Frame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.configure(background='#D0104C')
+        self.img = Image.open('panda.jpg')
+        self.panda = ImageTk.PhotoImage(self.img)
+        pandaLabel = tk.Label(self,image=self.panda)
+        pandaLabel.place(relx = 0.5, 
+                   rely = 0.5,
+                   anchor = 'center')
         self.pack(expand=1, fill='both')
 
 def main():
---
+++ lesson18/index3.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+class Window(tk.Tk):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.geometry('400x250+300+300')
+        self.title('Alignment')
+        self.configure(background='#F4A7B9')
+    
+class MyFrame(ttk.LabelFrame):
+    def __init__(self, master, title, **kwargs):
+        super().__init__(master, text=title, **kwargs)
+        self.alignment = tk.StringVar(value='left')
+        ttk.Radiobutton(self, text='左邊', value='left', variable=self.alignment, command=self.selected).grid(row=0, column=0, padx=20)
+        ttk.Radiobutton(self, text='中間', value='center', variable=self.alignment, command=self.selected).grid(row=0, column=1, padx=20)
+        ttk.Radiobutton(self, text='右邊', value='right', variable=self.alignment, command=self.selected).grid(row=0, column=2, padx=20)
+        self.pack()
+    
+    def selected(self):
+        print(self.alignment.get())
+
+
+def main():
+    window = Window()
+    myFrame = MyFrame(window, '對齊方式')
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
