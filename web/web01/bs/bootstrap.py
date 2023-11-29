c4b4cbf14a4c87abb90b6d26394a8dacaca813ec
Eddie Hsiao <xcvb1321@gmail.com>
2023-11-29 15:00:21 +0800

    modify

--- web/web01/bs/bootstrap.py
+++ web/web01/bs/bootstrap.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template
+from . import datasource
+import pandas as pd
 
 bp = Blueprint('bs', __name__, url_prefix='/bs')
 
 @bp.route('/')
 def index():
     return render_template('bs/index.html')
+
+@bp.route('/test1')
+def test1():
+    data:list[tuple] = datasource.lastest_datetime_data()
+    dataFrame = pd.DataFrame(data)
+    print(dataFrame)
+    return render_template('bs/test1.html')
 
 @bp.route('/product')
 def product():
